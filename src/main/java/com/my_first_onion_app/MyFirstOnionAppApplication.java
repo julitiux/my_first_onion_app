5d2e3406aaba491e363c001c9b139a569556c777
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-09-06 19:46:59 -0600

    first commit on my_first_onion_app

---
+++ src/main/java/com/my_first_onion_app/MyFirstOnionAppApplication.java
@@ -0,0 +1,13 @@
+package com.my_first_onion_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstOnionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstOnionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my_first_onion_app/MyFirstOnionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_first_onion_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstOnionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
