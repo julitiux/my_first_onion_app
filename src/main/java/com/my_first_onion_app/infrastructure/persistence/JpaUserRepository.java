6dd9816498585e9be24037f75bd8af6c3879ccc4
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-10-02 01:08:20 -0600

    implement the interface UserRepository

--- src/main/java/com/my_first_onion_app/infrastructure/persistence/JpaUserRepository.java
+++ src/main/java/com/my_first_onion_app/infrastructure/persistence/JpaUserRepository.java
@@ -1,4 +1,19 @@
 package com.my_first_onion_app.infrastructure.persistence;
 
-public class JpaUserRepository {
+import com.my_first_onion_app.domain.entity.User;
+import com.my_first_onion_app.domain.repository.UserRepository;
+
+import java.util.Optional;
+
+public class JpaUserRepository implements UserRepository {
+
+  @Override
+  public Optional<User> findByUsername(String username) {
+    return Optional.empty();
+  }
+
+  @Override
+  public User save(User user) {
+    return null;
+  }
 }
