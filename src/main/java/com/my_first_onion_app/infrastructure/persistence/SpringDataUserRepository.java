e74862a7e6fff03a45c772fbd31bfc54913768b8
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-10-02 01:06:18 -0600

    extends from JpaRepository<User,UUID>

--- src/main/java/com/my_first_onion_app/infrastructure/persistence/SpringDataUserRepository.java
+++ src/main/java/com/my_first_onion_app/infrastructure/persistence/SpringDataUserRepository.java
@@ -1,4 +1,9 @@
 package com.my_first_onion_app.infrastructure.persistence;
 
-public interface SpringDataUserRepository {
+import com.my_first_onion_app.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface SpringDataUserRepository extends JpaRepository<User, UUID> {
 }
