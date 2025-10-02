84c2480acf3303d58a9821e75ff96064afbb122f
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-10-01 23:04:49 -0600

    add method findByUsername

--- src/main/java/com/my_first_onion_app/domain/repository/UserRepository.java
+++ src/main/java/com/my_first_onion_app/domain/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.my_first_onion_app.domain.repository;
 
+import com.my_first_onion_app.domain.entity.User;
+
+import java.util.Optional;
+
 public interface UserRepository {
+
+  Optional<User> findByUsername(final String username);
+
 }
