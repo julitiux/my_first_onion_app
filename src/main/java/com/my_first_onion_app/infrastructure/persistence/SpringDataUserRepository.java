b763e3fad10e852ae24849d46621097a68bd2537
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-10-06 23:46:04 -0600

    declare the method findByUsername into SpringDataUserRepository

--- src/main/java/com/my_first_onion_app/infrastructure/persistence/SpringDataUserRepository.java
+++ src/main/java/com/my_first_onion_app/infrastructure/persistence/SpringDataUserRepository.java
@@ -3,7 +3,11 @@ package com.my_first_onion_app.infrastructure.persistence;
 import com.my_first_onion_app.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface SpringDataUserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findByUsername(String username);
+
 }
