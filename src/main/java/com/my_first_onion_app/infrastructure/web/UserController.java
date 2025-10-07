f0a6b819d15307df93cae069f7e628d38e270a5d
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-10-07 00:02:06 -0600

    inyecting the bean UserApplicationService into UserController

--- src/main/java/com/my_first_onion_app/infrastructure/web/UserController.java
+++ src/main/java/com/my_first_onion_app/infrastructure/web/UserController.java
@@ -1,9 +1,16 @@
 package com.my_first_onion_app.infrastructure.web;
 
+import com.my_first_onion_app.application.service.UserApplicationService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  private final UserApplicationService userApplicationService;
+
+  public UserController(UserApplicationService userApplicationService) {
+    this.userApplicationService = userApplicationService;
+  }
 }
