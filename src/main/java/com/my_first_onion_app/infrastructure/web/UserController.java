e56e3a897fb8b9f62d8f435afced602830e187c6
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-10-07 00:11:13 -0600

    implement method getUserByUsername

--- src/main/java/com/my_first_onion_app/infrastructure/web/UserController.java
+++ src/main/java/com/my_first_onion_app/infrastructure/web/UserController.java
@@ -1,6 +1,8 @@
 package com.my_first_onion_app.infrastructure.web;
 
 import com.my_first_onion_app.application.service.UserApplicationService;
+import com.my_first_onion_app.domain.entity.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +14,11 @@ public class UserController {
 
   public UserController(UserApplicationService userApplicationService) {
     this.userApplicationService = userApplicationService;
+  }
+
+  @GetMapping("/username")
+  public User getUserByUsername(String username) {
+    return userApplicationService.findByUsername(username)
+      .orElse(null);
   }
 }
