a3ae9de1b00d352d378046e8ad9668afbe89e4b3
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-10-06 23:52:49 -0600

    add RequestMapping starting with '/users' path

--- src/main/java/com/my_first_onion_app/infrastructure/web/UserController.java
+++ src/main/java/com/my_first_onion_app/infrastructure/web/UserController.java
@@ -1,4 +1,9 @@
 package com.my_first_onion_app.infrastructure.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 }
