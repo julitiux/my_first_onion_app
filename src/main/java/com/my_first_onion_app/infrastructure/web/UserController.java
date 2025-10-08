aec30fb5ae9231521b698aba2ff6d7092c023492
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-10-08 00:49:04 -0600

    implement controller UserController with a mapping '/users'

---
+++ src/main/java/com/my_first_onion_app/infrastructure/web/UserController.java
@@ -0,0 +1,10 @@
+package com.my_first_onion_app.infrastructure.web;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+}
