c44c5d9732cdd1f98048c5f887950f5831508493
Julio Ramirez <rrodriguez.julio@gmail.com>
2025-10-08 00:46:53 -0600

    implement method using the bean UserRepository applied on method like findByUsername and save

--- src/main/java/com/my_first_onion_app/domain/service/UserService.java
+++ src/main/java/com/my_first_onion_app/domain/service/UserService.java
@@ -1,7 +1,26 @@
 package com.my_first_onion_app.domain.service;
 
+import com.my_first_onion_app.domain.entity.User;
+import com.my_first_onion_app.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
+
+  private final UserRepository userRepository;
+
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public Optional<User> findByUsername(String username) {
+    return userRepository.findByUsername(username);
+  }
+
+  public User save(User user) {
+    return userRepository.save(user);
+  }
+
 }
